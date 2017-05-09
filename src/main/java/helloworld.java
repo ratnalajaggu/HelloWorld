cf87eba14fb25f07c2c60faceac4045bdf7c6223
Jaggu <ratnalajagadeesh123@gmail.com>
2017-05-09 11:36:03 +0530

    First commit

---
+++ src/main/java/helloworld.java
@@ -0,0 +1,8 @@
+import org.apache.log4j.Logger;
+public class helloworld {
+  Logger logger;
+  public static void main(String[] args){
+    System.out.println("Hello world Jaggu");
+    System.out.println("Hello world Jagadeesh");
+  }
+}
